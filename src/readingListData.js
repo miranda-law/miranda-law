08f331467e0d07c6bbcb9e4d3c5e3200fdf22875
Miranda <miranda_law@outlook.com>
2024-02-18 14:38:18 -0800

    create readingListData, add mdzs

---
+++ src/readingListData.js
@@ -0,0 +1,16 @@
+const readingListData = [
+    {
+        "name": "Mo Dao Zu Shi",
+        "medium": "manhua",
+        "altNames": [
+            "Grandmaster of Demonic Cultivation",
+            "Master of Diabolism"],
+        "author": "Mo Xiang Tong Xiu",
+        "status": "complete",
+        "tags": ["bl", "xianxia", "romance", "mystery", "comedy", "mature", "reincarnation"],
+        "officialEn": "https://sevenseasentertainment.com/series/grandmaster-of-demonic-cultivation-mo-dao-zu-shi-the-comic-manhua/",
+        "comments": "one of my favorite series of all time, great even without the bl, tightknit story and well written characters"
+    }
+]
+
+export default readingListData
