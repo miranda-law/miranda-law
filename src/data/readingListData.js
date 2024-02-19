20d1e4a38feecf05017490aa3f9435e5aa78e838
Miranda <miranda_law@outlook.com>
2024-02-18 20:48:39 -0800

    create data folder

--- src/data/readingListData.js
+++ src/data/readingListData.js
@@ -9,6 +9,7 @@ const readingListData = [
         "status": "complete",
         "tags": ["bl", "xianxia", "romance", "mystery", "comedy", "mature", "reincarnation"],
         "officialEn": "https://sevenseasentertainment.com/series/grandmaster-of-demonic-cultivation-mo-dao-zu-shi-the-comic-manhua/",
+        "imgLink": "",
         "comments": "one of my favorite series of all time, great even without the bl, tightknit story and well written characters"
     }
 ]
