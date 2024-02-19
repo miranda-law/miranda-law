8f407ee693f95b1fc6e086b4e3e1baf04fd846f6
Miranda <miranda_law@outlook.com>
2024-02-19 00:50:20 -0800

    rename Navbar to NavigationBar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {Outlet} from "react-router-dom"; 
-import Navbar from "../Navbar"
+import NavigationBar from "../NavigationBar"
 
 const Layout = () => {   
     return (
     <>
-        <Navbar />
+        <NavigationBar />
         <Outlet />
     </>   ); 
 };
