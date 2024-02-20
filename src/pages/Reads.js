a2a10db34bf7a3373353571169fc96eb26952e20
Miranda <miranda_law@outlook.com>
2024-02-19 22:13:51 -0800

    import reading data and change Reads page to class

--- src/pages/Reads.js
+++ src/pages/Reads.js
@@ -1,9 +1,47 @@
 import React from "react";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import MediaItem from "../components/MediaItem";
+import mangaData from '../data/readings/mangaData';
+import manhwaData from '../data/readings/manhwaData';
+import webtoonData from '../data/readings/webtoonData';
+import manhuaData from '../data/readings/manhuaData';
+import animeData from '../data/readings/animeData';
 
-function Reads() {
-    return (
-        <div className="reads-container">Reading List</div>
-    );
+
+class Reads extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            mangas: mangaData,
+            manhwas: manhwaData,
+            webtoons: webtoonData,
+            manhuas: manhuaData,
+            animes: animeData
+        }
+    }
+    render() {
+        const manhuaItems = this.state.manhuas.map(
+            item => <MediaItem key={item.title} item={item} />
+        );
+
+        return (
+            <div className="reads-container">
+                Reading List
+                {/* {manhuaItems} */}
+
+                <Row xs={1} md={4} className="g-4">
+                    {manhuaItems}
+                    {/* {Array.from({ length: manhuaItems.length }).map((_, idx) => (
+                        <Col key={idx}>
+                            {manhuaItems}
+                        </Col>
+                    ))} */}
+                </Row>
+            </div>
+        );
+    }
+    
 }
 
 export default Reads
