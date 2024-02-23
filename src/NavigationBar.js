c222dd21374c8774d1ed30e8610af00f3d574b0b
Miranda <miranda_law@outlook.com>
2024-02-22 18:49:22 -0800

    add dropdown to reading list on navbar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import './NavigationBar.css';
 
 function NavigationBar() {
@@ -7,7 +8,16 @@ function NavigationBar() {
         <nav>
           <ul>
             <li><Link to="/miranda-law">Miranda Law</Link></li>
-            <li className="right"><Link to="reads">Reading List</Link></li>
+            <li className="right" id="dropdown">
+              <Link to="reads">Reading List</Link>
+              <div className="dropdown-content">
+                <HashLink to="/miranda-law/reads#manga-header">Manga</HashLink>
+                <HashLink to="/miranda-law/reads#manhwa-header">Manhwa</HashLink>
+                <HashLink to="/miranda-law/reads#webtoon-header">Webtoon</HashLink>
+                <HashLink to="/miranda-law/reads#manhua-header">Manhua</HashLink>
+                <HashLink to="/miranda-law/reads#anime-header">Anime</HashLink>
+              </div>
+            </li>
             <li className="right"><Link to="projects">Projects</Link></li>
             <li className="right"><Link to="/miranda-law">Home</Link></li>
           </ul>
