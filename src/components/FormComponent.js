8682a63d96c76176bed1b646957b5528fde8f672
Miranda <miranda_law@outlook.com>
2024-02-27 16:50:29 -0800

    enable status and tag filtering

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -2,8 +2,6 @@ import React from "react";
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import { default as funnel } from '../assets/funnel.svg';
 import "./FormComponent.css";
 
@@ -21,11 +19,11 @@ function FormComponent(props) {
                     id="funnel" 
                 />
             </button>
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show} onHide={handleClose} size="lg">
                 <Modal.Header closeButton>
                     <Modal.Title>Filter</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
+                <Modal.Body className="modal-form-body">
                     <form>
                         
                         <label className="form-label" htmlFor="form-status">Status:</label> <br />
@@ -35,8 +33,8 @@ function FormComponent(props) {
                                 type="radio" 
                                 name="form-status"
                                 value="all"
-                                checked={props.item.seriesStatus === "all"}
-                                onChange={props.handleFormStatus}
+                                checked={props.item.displayStatus === "all"}
+                                onChange={props.handleChange}
                             /> All
                         </label>
                     
@@ -45,8 +43,8 @@ function FormComponent(props) {
                                 type="radio" 
                                 name="form-status"
                                 value="complete"
-                                checked={props.item.seriesStatus === "complete"}
-                                onChange={props.handleFormStatus}
+                                checked={props.item.displayStatus === "complete"}
+                                onChange={props.handleChange}
                             /> Complete
                         </label>
                     
@@ -55,8 +53,8 @@ function FormComponent(props) {
                                 type="radio" 
                                 name="form-status"
                                 value="ongoing"
-                                checked={props.item.seriesStatus === "ongoing"}
-                                onChange={props.handleFormStatus}
+                                checked={props.item.displayStatus === "ongoing"}
+                                onChange={props.handleChange}
                             /> Ongoing
                         </label>
                     
@@ -65,11 +63,29 @@ function FormComponent(props) {
                                 type="radio" 
                                 name="form-status"
                                 value="hiatus"
-                                checked={props.item.seriesStatus === "hiatus"}
-                                onChange={props.handleFormStatus}
+                                checked={props.item.displayStatus === "hiatus"}
+                                onChange={props.handleChange}
                             /> Hiatus
-                        </label>
+                        </label> <br /> <br />
                         
+                        <label className="form-label" htmlFor="form-tags">Tags:</label>
+
+                        <div className="form-tag-container">
+                            {Object.keys(props.item.displayTags).map((item) => {
+                                return (
+                                <>
+                                    <label key={`checkbox-${item}`}className="checkbox-choice">
+                                        <input 
+                                            type="checkbox" 
+                                            name="form-tags"
+                                            value={item}
+                                            checked={props.item.displayTags[item]}
+                                            onChange={props.handleChange}
+                                        /> {item}
+                                    </label> <br />
+                                </>)
+                            })}
+                        </div>
                     </form>
                 </Modal.Body>
                 <Modal.Footer>
--- src/pages/Reads.js
+++ src/pages/Reads.js
@@ -9,6 +9,7 @@ import manhwaData from '../data/readings/manhwaData';
 import webtoonData from '../data/readings/webtoonData';
 import manhuaData from '../data/readings/manhuaData';
 import animeData from '../data/readings/animeData';
+import allTags from "../data/readings/readingListTags";
 
 
 class Reads extends React.Component {
@@ -20,45 +21,119 @@ class Reads extends React.Component {
             webtoons: webtoonData,
             manhuas: manhuaData,
             animes: animeData,
-            seriesStatus: "all"
-
+            displayStatus: "all",
+            displayTags: {}
         }
-        this.handleFormStatus = this.handleFormStatus.bind(this)
-    }
-    handleFormStatus(event) {
-        const {name, value, type, checked} = event.target
-        this.setState({seriesStatus: value});
-        value === "all" ? 
-            this.setState({
-                mangas: mangaData,
-                manhwas: manhwaData,
-                webtoons: webtoonData,
-                manhuas: manhuaData,
-                animes: animeData,
-            })
-        :
-        this.setState({
-            mangas: mangaData.filter(item => item.status === value),
-            manhwas: manhwaData.filter(item => item.status === value),
-            webtoons: webtoonData.filter(item => item.status === value),
-            manhuas: manhuaData.filter(item => item.status === value),
-            animes: animeData.filter(item => item.status === value)
+        this.handleChange = this.handleChange.bind(this)
+        // set up displayTags
+        let tempTags = {};
+        allTags.forEach((item) => {
+            tempTags[item] = false;
         })
+        this.state.displayTags = tempTags;
+        
     }
+    
+    handleChange(event) {
+        const {value, type} = event.target;
+        let updatedMangas = mangaData;
+        let updatedManhwas = manhwaData;
+        let updatedWebtoons = webtoonData;
+        let updatedManhuas = manhuaData;
+        let updatedAnimes = animeData;
+        if(type === "radio") {
+            // update new status
+            this.setState({displayStatus: value});
+            // identify valid tags
+            const validTags = Object.keys(this.state.displayTags).filter(item => this.state.displayTags[item]);
+            // filter by tags
+            if(validTags.length > 0) {
+                updatedMangas = updatedMangas.filter(
+                item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                );
+                updatedManhwas = updatedManhwas.filter(
+                    item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                );
+                updatedWebtoons = updatedWebtoons.filter(
+                    item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                );
+                updatedManhuas = updatedManhuas.filter(
+                    item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                );
+                updatedAnimes = updatedAnimes.filter(
+                    item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                );
+            }
+            // filter by status and set state
+            if(value === "all") {
+                this.setState({
+                    mangas: updatedMangas,
+                    manhwas: updatedManhwas,
+                    webtoons: updatedWebtoons,
+                    manhuas: updatedManhuas,
+                    animes: updatedAnimes
+                })
+            } else {
+                this.setState({
+                    mangas: updatedMangas.filter(item => item.status === value),
+                    manhwas: updatedManhwas.filter(item => item.status === value),
+                    webtoons: updatedWebtoons.filter(item => item.status === value),
+                    manhuas: updatedManhuas.filter(item => item.status === value),
+                    animes: updatedAnimes.filter(item => item.status === value)
+                })
+            }
 
-    handleFormTags(event) {
-        const {name, value, type, checked} = event.target
-        this.setState({seriesStatus: value});
-        value === "all" ? 
-            this.setState({
-                mangas: mangaData
+        } else if (type === "checkbox") {
+            this.setState(prevState => {
+                // update valid tags
+                const updatedTags = prevState.displayTags;
+                updatedTags[value] = !prevState.displayTags[value];
+                // identify valid tags
+                const validTags = Object.keys(updatedTags).filter(item => this.state.displayTags[item]);
+                // filter by status
+                if(prevState.displayStatus !== "all") {
+                    updatedMangas = updatedMangas.filter(item => item.status === prevState.displayStatus);
+                    updatedManhwas = updatedManhwas.filter(item => item.status === prevState.displayStatus);
+                    updatedWebtoons = updatedWebtoons.filter(item => item.status === prevState.displayStatus);
+                    updatedManhuas = updatedManhuas.filter(item => item.status === prevState.displayStatus);
+                    updatedAnimes = updatedAnimes.filter(item => item.status === prevState.displayStatus);
+                }
+                // filter by tags and set state
+                if(validTags.length > 0) {
+                    return {
+                        displayTags: updatedTags,
+                        mangas: updatedMangas.filter(
+                            item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                        ),
+                        manhwas: updatedManhwas.filter(
+                            item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                        ),
+                        webtoons: updatedWebtoons.filter(
+                            item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                        ),
+                        manhuas: updatedManhuas.filter(
+                            item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                        ),
+                        animes: updatedAnimes.filter(
+                            item => item.tags.filter(value => validTags.includes(value)).length === validTags.length
+                        )
+                    };
+                } else { // no tags to filter
+                    return {
+                        displayTags: updatedTags,
+                        mangas: updatedMangas,
+                        manhwas: updatedManhwas,
+                        webtoons: updatedWebtoons,
+                        manhuas: updatedManhuas,
+                        animes: updatedAnimes
+                    }
+                }
             })
-        :
-        this.setState({
-            mangas: mangaData.filter(item => item.status === value)
-        })
+        }
+
     }
     render() {
+        //console.log(this.state.mangas)
         const mangaItems = this.state.mangas.map(
             item => <MediaItem key={item.title} item={item} />
         );
@@ -89,9 +164,10 @@ class Reads extends React.Component {
                 <Row><h1 id="anime-header">Anime</h1></Row>
                 <Row>{animeItems}</Row>
                 <FormComponent 
-                    key="formButton" 
                     item={this.state} 
-                    handleFormStatus={this.handleFormStatus} 
+                    handleFormStatus={this.handleFormStatus}
+                    handleFormTags={this.handleFormTags}
+                    handleChange={this.handleChange}
                 />
             </Container>
 
