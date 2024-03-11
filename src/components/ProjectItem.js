78399f590e233345955bfbaba2cc4d37c29cdbe5
Miranda <miranda_law@outlook.com>
2024-03-11 01:04:59 -0700

    create ProjectItem component and add css

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {ReactComponent as External} from '../assets/external.svg';
+import "./ProjectItem.css"
+
+function ProjectItem(props) {
+    const data = props.item;
+    const k = props.dataKey;
+    return (
+        <div className="project-item-container">
+            <a href={data.link} target="_blank">
+                <h2 className="project-title">
+                    {data.project} {" "}
+                    <External className="funnel" />
+                </h2>
+                </a>
+                <p className="project-desc">{data.description}</p>
+                <div className="project-stack-container">
+                    {data.stack.map((item, i) => {
+                        return (
+                            <span className="stack-tag" key={`${k}_tag${i}`}>
+                                {item}
+                            </span>  
+                        )
+                    })}
+                </div>
+            
+        </div>
+    );
+}
+
+export default ProjectItem;
