2a2b2bb67937c49615fee42b32772de68e5dd8f3
Miranda <miranda_law@outlook.com>
2024-03-11 01:05:51 -0700

    create Project page to start adding content

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,30 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import ProjectItem from "../components/ProjectItem";
+import projectData from '../data/projects/project-list.json';
 
-function Projects() {
-    return (
-        <div className="projects-container">Projects</div>
-    );
+class Projects extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: projectData
+        };
+    }
+    render() {
+        const projectItems = this.state.projects.map((item, i) => 
+            <Row key={`project-row${i}`}>
+                <ProjectItem dataKey={`project${i}`} item={item} />
+            </Row>
+        )
+        return(
+            <Container fluid style={{maxWidth: "60%"}}>
+                <Row>
+                    <h1 id="projects">Projects</h1>
+                </Row> <br/>
+                {projectItems}
+            </Container>
+        );
+    }
 }
-
 export default Projects
